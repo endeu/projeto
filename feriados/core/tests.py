b91532570d708462d1e65e30a2e4e7f08f8ec050
Wendell <wendellpguariento@gmail.com>
2022-06-22 21:51:30 -0300

    realizado migrações e atualizado teste para db

--- feriados/core/tests.py
+++ feriados/core/tests.py
@@ -1,6 +1,8 @@
+from core.models import FeriadoModel
+from datetime import datetime
 from django.test import TestCase
-class NatalTest(TestCase):
-    
+
+class FeriadoTest(TestCase):    
     def setUp(self):
         self.resp = self.client.get('/')
 
@@ -12,3 +14,29 @@ class NatalTest(TestCase):
         
     def test_template_feriado(self):
         self.assertTemplateUsed(self.resp, 'feriado.html')
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+            nome=self.feriado,
+            dia=self.dia,
+            mes=self.mes,
+        )
+        self.cadastro.save()
+
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+        
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.feriado)
+        
+    def test_dia_feriado(self):
+        dia = self.cadastro.__dict__.get('dia', '')
+        self.assertEqual(dia, self.dia)
